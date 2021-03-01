2bbf7687569f4d7046b88fffade789a2ec993a8e
aditPrayogo <adit.ihzar@gmail.com>
2021-03-01 20:05:31 +0700

    adding transition animation

--- app/src/main/java/com/example/android_pemula_traditional_dance/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/android_pemula_traditional_dance/ui/main/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.android_pemula_traditional_dance.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_pemula_traditional_dance.R
 import com.example.android_pemula_traditional_dance.data.TraditionalDanceData
 import com.example.android_pemula_traditional_dance.databinding.ActivityMainBinding
+import com.example.android_pemula_traditional_dance.ui.profile.ProfileActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,5 +37,17 @@ class MainActivity : AppCompatActivity() {
             listDanceAdapter.setActivity(this@MainActivity)
             adapter = listDanceAdapter
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.idMenuProfile -> startActivity(Intent(this, ProfileActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
